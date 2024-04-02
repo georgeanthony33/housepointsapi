b71926eace1f280ac68bbb8f15e1c035cbda521b
George Anthony <george.anthony@kantan.tech>
2024-04-02 19:09:36 +0100

    initial setup

---
+++ src/main/java/com/housepoints/housepointsapi/HousepointsapiApplication.java
@@ -0,0 +1,13 @@
+package com.housepoints.housepointsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousepointsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousepointsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/housepoints/housepointsapi/HousepointsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.housepoints.housepointsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousepointsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
